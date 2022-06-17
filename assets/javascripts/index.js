a4c76f36c356a06064e1335c4a2cda0f06ba5e17
Annu Kumar Singh <annukumarsingh.aks@gmail.com>
2022-06-17 13:17:05 +0530

    Created search bar resulty

--- assets/javascripts/index.js
+++ assets/javascripts/index.js
@@ -1,3 +1,34 @@
+function searchMovies(searchString){
+
+    let moviesList;
+    // url api link
+    let url = `https://www.omdbapi.com/?s=${searchString}&apikey=3b9420f8`      
+    let movies = fetch(url).then((response)=> response.json()).then((data)=>data);
+    console.log(movies);
+
+    // fetching url api
+    fetch(url)
+    .then((response) => {
+        return response.json();
+    })                                      
+    .then((data) => {
+        console.log(data);
+        moviesList = data;
+    }, true);
+    return moviesList;
+}
+searchMovies();
+
+
+
+
+
+
+
+
+
+
+
 function closeTrailor(){
     document.getElementById("trailer-play-container").style.display = "none";
 }
